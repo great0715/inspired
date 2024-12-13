c35d4212f23ccd3b06b3f5d4ef92622026087982
Inspired <jd3606192@gmail.com>
2024-12-13 09:05:31 -0500

    latest update

--- mysql/actions.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("config.php");
-require_once("functions.php");
-
-if (isset($_POST['action'])) {
-    $action = $_POST['action'];
-    unset($_POST['action']);
-    $call = call_user_func($action, $_POST);
-}
